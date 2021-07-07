8077541d2908c89bde03d9d88d953754329b1dcf
Julien ROSSI <j.rossi@uva.nl>
2021-07-07 16:22:35 +0200

    :sparkles::memo::white_check_mark: add new iterators
    * iterators for elasticsearch (need elasticsearch_dsl) and parquet datasets (need pyarrow)
    * added tests
    * cleaned code documentation
    * added dev-dependencies

--- ez_parallel/misc.py
+++ ez_parallel/misc.py
@@ -4,6 +4,14 @@ import logging
 
 
 def list_iterator(items: List[Any]) -> Tuple[Callable[[], Iterator[Any]], int]:
+    """
+    Build a generator from a list.
+    The created iterator will yield the items of the list one by one.
+
+    :param items: list of items to process
+    :return: Tuple generator, number of items
+    """
+
     def _iterator():
         yield from items
 
@@ -13,14 +21,26 @@ def list_iterator(items: List[Any]) -> Tuple[Callable[[], Iterator[Any]], int]:
 def batch_iterator_from_sliceable(
     items: Any, batch_size: int
 ) -> Callable[[], Iterator[Any]]:
+    """
+    Build a generator of batches from any object that can be sliced.
+    The created iterator will yield the items of the list by batches.
+    Each call to next() will return a batch, i.e. a list of batch_size items.
+    The last batch might contain less than batch_size items.
+
+    :param items: list of items
+    :param batch_size: number of items in a batch
+    :return: generator
+    """
+
     def _iterator():
         batch_id = 0
         while True:
-            logging.info("Get one batch")
             batch = items[batch_id * batch_size : (batch_id + 1) * batch_size]
             if len(batch) > 0:
+                logging.info("Get one batch")
                 yield batch
                 if len(batch) < batch_size:
+                    logging.info("Last batch")
                     break
             batch_id += 1
 
@@ -30,6 +50,17 @@ def batch_iterator_from_sliceable(
 def batch_iterator_from_iterable(
     items: Iterable[Any], batch_size: int
 ) -> Callable[[], Iterator[Any]]:
+    """
+    Build a generator of batches from an iterable.
+    The created iterator will yield the items of the list by batches.
+    Each call to next() will return a batch, i.e. a list of items.
+    The last batch might contain less than batch_size items.
+
+    :param items: iterable yielding items
+    :param batch_size: number of items in a batch
+    :return: generator
+    """
+
     def _iterator():
         while True:
             logging.info("Get one batch")
@@ -44,6 +75,16 @@ def batch_iterator_from_iterable(
 def batch_iterator(
     items: List[Any], batch_size: int
 ) -> Tuple[Callable[[], Iterator[Any]], int]:
+    """
+    Build a generator of batches from a list of items.
+    The created iterator will yield the items of the list by batches.
+    Each call to next() will return a batch, i.e. a list of items.
+    The last batch might contain less than batch_size items.
+
+    :param items: list of items
+    :param batch_size: number of items in a batch
+    :return: Tuple generator, number of items (NOT number of batches)
+    """
     list_iter_fn, _ = list_iterator(items)
     list_iter = list_iter_fn()
 
@@ -55,3 +96,100 @@ def batch_iterator(
                 break
 
     return _iterator, len(items)
+
+
+try:
+    import elasticsearch_dsl
+
+    def elasticsearch_iterator(
+        s: elasticsearch_dsl.Search,
+    ) -> Tuple[Callable[[], Iterator[Any]], int]:
+        """
+        Build a generator items retrieved from ElasticSearch from a Search object.
+        The created iterator will yield the results of the search one by one
+        When the Search object is incorrect (no connection, no index, ...), exceptions will be raised, and execution
+        will stop.
+
+        :param s: Search object
+        :return: Tuple generator, number of items
+        """
+        count = s.count()
+
+        def _iterator():
+            scan = s.scan()
+            yield from scan
+
+        return _iterator, count
+
+    def elasticsearch_batch_iterator(
+        s: elasticsearch_dsl.Search, batch_size: int
+    ) -> Tuple[Callable[[], Iterator[Any]], int]:
+        """
+        Build a generator of items retrieved from ElasticSearch from a Search object.
+        The created iterator will yield the results of the search in batches.
+        Each call to next() will return a batch, i.e. a list of items.
+        The last batch might contain less than batch_size items.
+        When the Search object is incorrect (no connection, no index, ...), exceptions will be raised, and execution
+        will stop.
+
+        :param s: Search object
+        :param batch_size: number of items per batch
+        :return: Tuple generator, number of items
+        """
+        generator, count = elasticsearch_iterator(s)
+        iterable = generator()
+
+        batch_generator = batch_iterator_from_iterable(iterable, batch_size)
+
+        return batch_generator, count
+
+
+except ImportError:
+
+    def elasticsearch_iterator(*_, **__):
+        raise NotImplementedError(
+            "Install elasticsearch_dsl in order to use the function elasticsearch_iterator"
+        )
+
+    def elasticsearch_batch_iterator(*_, **__):
+        raise NotImplementedError(
+            "Install elasticsearch_dsl in order to use the function elasticsearch_batch_iterator"
+        )
+
+
+try:
+    import pyarrow as pa
+    import pyarrow.dataset as ds
+    import pyarrow.parquet as pq
+
+    def parquet_dataset_batch_iterator(
+        dataset: ds.FileSystemDataset, batch_size: int
+    ) -> Tuple[Callable[[], Iterator[Any]], int]:
+        """
+        Build a generator of items retrieved from a Parquet Dataset.
+        The created iterator will yield the items in batches.
+        Each call to next() will return a batch, i.e. an object pyarrow.RecordBatch.
+        Batches will not all have the same size.
+
+        :param dataset: a Parquet Dataset object
+        :param batch_size: number of items per batch
+        :return: Tuple generator, number of items (NOT number of batches)
+        """
+
+        def _iterator():
+            if pa.__version__ < "4.0.0":
+                for scan_task in dataset.scan(batch_size=batch_size):
+                    yield from scan_task.execute()
+            else:
+                yield from dataset.to_batches(batch_size=batch_size)
+
+        nb_rows = sum(pq.read_metadata(f).num_rows for f in dataset.files)
+        return _iterator, nb_rows
+
+
+except ImportError:
+
+    def parquet_dataset_batch_iterator(*_, **__):
+        raise NotImplementedError(
+            "Install pyarrow in order to use the function parquet_dataset_batch_iterator"
+        )
--- tests/test_ez_multiprocess/test_ez_multiprocess.py
+++ tests/test_ez_multiprocess/test_ez_multiprocess.py
@@ -1,3 +1,120 @@
+from typing import Any
+
+import random
+import tempfile
+
+import pandas as pd
+import pyarrow as pa
+import pyarrow.dataset as ds
+from ez_parallel import (
+    batch_iterator,
+    batch_iterator_from_iterable,
+    batch_iterator_from_sliceable,
+    elasticsearch_batch_iterator,
+    elasticsearch_iterator,
+    list_iterator,
+    parquet_dataset_batch_iterator,
+)
+
+
+def test_batch_iterator():
+    num_items = random.randint(1000, 1500)
+    batch_size = random.randint(64, 128)
+
+    data = [random.random() for _ in range(num_items)]
+    gen, count = batch_iterator(data, batch_size)
+
+    assert count == num_items
+
+    iterable = gen()
+    batch = next(iterable)
+
+    assert isinstance(batch, list)
+    assert len(batch) == batch_size
+
+
+def test_batch_iterator_from_iterable():
+    num_items = random.randint(1000, 1500)
+    batch_size = random.randint(64, 128)
+
+    data = (random.random() for _ in range(num_items))
+    gen = batch_iterator_from_iterable(data, batch_size)
+
+    iterable = gen()
+    batch = next(iterable)
+
+    assert isinstance(batch, list)
+    assert len(batch) == batch_size
+
+
+def test_batch_iterator_from_sliceable():
+    num_items = random.randint(1000, 1500)
+    batch_size = random.randint(64, 128)
+
+    data = [random.random() for _ in range(num_items)]
+    gen = batch_iterator_from_sliceable(data, batch_size)
+
+    iterable = gen()
+    batch = next(iterable)
+
+    assert isinstance(batch, list)
+    assert len(batch) == batch_size
+
+
+def test_elasticsearch_iterator():
+    """
+    No automatic test. Tried and working.
+    :return:
+    """
+    pass
+
+
+def test_elasticsearch_batch_iterator():
+    """
+    No automatic test. Tried and working.
+    :return:
+    """
+    pass
+
+
+def test_list_iterator():
+    num_items = random.randint(1000, 1500)
+
+    data = [random.random() for _ in range(num_items)]
+    gen, count = list_iterator(data)
+
+    assert count == num_items
+
+    iterable = gen()
+    item = next(iterable)
+
+    assert isinstance(item, float)
+
+
+def test_parquet_dataset():
+    num_rows = 1000
+    batch_size = 64
+
+    data = [
+        {"a": random.random(), "b": random.randint(1, 20)}
+        for _ in range(num_rows)
+    ]
+
+    with tempfile.NamedTemporaryFile("w") as tmp:
+        pd.DataFrame(data).to_parquet(tmp.name, engine="pyarrow")
+        dataset: Any = ds.dataset(tmp.name)
+        dataset: ds.FileSystemDataset
+        gen, count = parquet_dataset_batch_iterator(dataset, batch_size)
+
+        assert count == num_rows
+
+        iterable = gen()
+        batch = next(iterable)
+
+        assert isinstance(batch, pa.RecordBatch)
+        assert batch.num_rows == batch_size
+
+
 def test_pass():
     """Sorry no test for the moment"""
     assert True
